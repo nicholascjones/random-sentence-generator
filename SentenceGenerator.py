c4e55198cedfd9247594d8552b674535b50fc14f
nicholascjones <njones7@nd.edu>
2016-02-24 21:03:29 -0500

    Started work on project

---
+++ SentenceGenerator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+## SentenceGenerator.py
+## Nicholas Jones
+## Course Project 1 -- English Sentence Generator
+
+#imports
+import random
+
+#functions
+
+#noun phrase takes no arguments as is first part of sentence
+def NounPhrase():
+
+	#establish random boolean value
+	rn = random.randint(0,1)
+
+	#if statement to determine
+	if rn == 0
+
+#List Initialization for Parts of Speech
+proper = []
+common = []
+modifier = []
+verb = []
+prep = []
+
+
+#Start with Noun Phrase
+sent = NounPhrase()
+
+
+
