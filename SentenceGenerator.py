a207979021b260b6d38b70301be23070dede44ff
nicholascjones <njones7@nd.edu>
2016-02-24 21:17:49 -0500

    Implemented proper noun phrase

--- SentenceGenerator.py
+++ SentenceGenerator.py
@@ -16,7 +16,23 @@ def NounPhrase():
 	rn = random.randint(0,1)
 
 	#if statement to determine
-	if rn == 0
+	if rn == 0: #if random int is zero, generate proper noun as phrase
+		n = random.randint(0,(len(proper)-1) #picks random index
+		phrase = proper[n] + " " #initializes to noun phrase plus space
+
+	else: #if random int is 1, generate common noun phrase
+		phrase = "The " #initializes phrase with "The"
+
+		rn = random.randint(0,1) #re-initializes random int
+		if rn == 1: #if random int is 1, need to include modifier
+			n = random.randint(0,(len(modifier)-1) #picks random index
+			phrase += (modifier[n] + " ") #appends modifier and space to end of string
+		
+		
+		
+
+	
+	return phrase #noun phrase returned here
 
 #List Initialization for Parts of Speech
 proper = []
